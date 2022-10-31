d255c8839e556ba8222a3f34a99a7c111a9caa93
Yuwen Xiong <yxiong@waabi.ai>
2022-10-31 00:32:43 +0000

    update

---
+++ configs/pointpillars/hv_pointpillars_secfpn_sbn_2x16_2x_waymoD2_front-3d-car.py
@@ -0,0 +1,48 @@
+_base_ = [
+    "../_base_/models/hv_pointpillars_secfpn_waymo2kitti.py",
+    "../_base_/datasets/waymoD2_front-3d-car.py",
+    "../_base_/schedules/schedule_2x.py",
+    "../_base_/default_runtime.py",
+]
+
+# model settings
+model = dict(
+    bbox_head=dict(
+        type="Anchor3DHead",
+        num_classes=1,
+        anchor_generator=dict(
+            type="AlignedAnchor3DRangeGenerator",
+            # ranges=[[-74.88, -74.88, -0.0345, 74.88, 74.88, -0.0345]],
+            # ranges=[[-74.88, -74.88, 0, 74.88, 74.88, 0]],
+            ranges=[[0, -40, 0, 75, 40, 0]],
+            # sizes=[[4.7, 2.1, 1.7]],
+            sizes=[[3.9, 1.6, 1.56]],
+            rotations=[0, 1.57],
+            reshape_out=True,
+        ),
+    ),
+    # model training and testing settings
+    train_cfg=dict(
+        _delete_=True,
+        assigner=dict(
+            type="MaxIoUAssigner",
+            iou_calculator=dict(type="BboxOverlapsNearest3D"),
+            pos_iou_thr=0.55,
+            neg_iou_thr=0.4,
+            min_pos_iou=0.4,
+            ignore_iof_thr=-1,
+        ),
+        allowed_border=0,
+        code_weight=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+        pos_weight=-1,
+        debug=False,
+    ),
+)
+data = dict(
+    samples_per_gpu=4,
+)
+runner = dict(type="EpochBasedRunner", max_epochs=24)
+optimizer = dict(type="AdamW", lr=0.0005, weight_decay=0.01)
+# work_dir = "work_dirs/test"
+# mp_start_method = "forkserver"
+custom_hooks = [dict(type="SyncAWSHook")]
